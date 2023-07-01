d2a00467d508b41af435fc16854845da52054df2
Carlos Campagnolo <ccampagnolos@gmail.com>
2023-07-01 12:35:47 +0200

    lab done

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
+mongoose.set('strictQuery', false);
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
@@ -17,7 +18,42 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    const recipe = {
+      title: 'Pizza',
+      level: 'Amateur Chef',
+      ingredients: ['Masa' ,'Salsa de Tomate', 'Queso'],
+      cuisine: 'Italian',
+      dishType: 'main_course',
+      duration: 25,
+      creator: 'Chef Cayo',
+    };
+    return Recipe.create(recipe);
   })
-  .catch(error => {
+  .then((createdRecipe) => {
+    console.log(`Recipe created: ${createdRecipe.title}`);
+
+    return Recipe.insertMany(data);
+  })
+  .then((insertedRecipes) => {
+    insertedRecipes.forEach((recipe) => {
+      console.log(`Recipe inserted: ${recipe.title}`);
+    });
+   
+    return Recipe.findOneAndUpdate(
+      { title: 'Rigatoni alla Genovese' },
+      { duration: 100 }
+    );
+  })
+  .then(() => {
+    console.log('Recipe updated successfully');
+    
+    return Recipe.deleteOne({ title: 'Carrot Cake' });
+  })
+  .then(() => {
+    console.log('Recipe deleted successfully');
+    
+    mongoose.connection.close();
+  })
+  .catch((error) => {
     console.error('Error connecting to the database', error);
   });
